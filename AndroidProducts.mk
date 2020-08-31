5251a77b5a46d59f5e76fcb910ada0f664c4be46
adiatul16 <adiatul16@gmail.com>
2020-08-31 13:25:13 +0530

    lavender: Adapt for Havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,11 +21,11 @@
 # definition file).
 #
 
-# Inherit Redmi Note 7 Carbon device config
+# Inherit Redmi Note 7 Havoc device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_lavender.mk
+    $(LOCAL_DIR)/havoc_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_lavender-eng \
-    carbon_lavender-userdebug \
-    carbon_lavender-user
+    havoc_lavender-eng \
+    havoc_lavender-userdebug \
+    havoc_lavender-user
